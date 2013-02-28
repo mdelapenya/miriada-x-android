2d6d92a43dde4e66afe8b79cb66d0736fd8d0e2e
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-02-28 20:09:00 +0100

    Adding styles

--- gen/org/example/asteroides/R.java
+++ gen/org/example/asteroides/R.java
@@ -63,5 +63,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int TextoAsteroides=0x7f050002;
+        public static final int TextoAsteroides_Botones=0x7f050003;
     }
 }
