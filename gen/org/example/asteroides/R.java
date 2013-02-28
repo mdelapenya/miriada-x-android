8ae5fae68c61b126dbbfe61f67599c0a4283bda7
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-02-28 20:24:18 +0100

    Adding AcercaDe activity

--- gen/org/example/asteroides/R.java
+++ gen/org/example/asteroides/R.java
@@ -14,14 +14,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f070000;
-        public static final int Button02=0x7f070001;
-        public static final int Button03=0x7f070002;
-        public static final int Button04=0x7f070003;
-        public static final int menu_settings=0x7f070004;
+        public static final int Button01=0x7f070001;
+        public static final int Button02=0x7f070002;
+        public static final int Button03=0x7f070003;
+        public static final int Button04=0x7f070004;
+        public static final int TextView01=0x7f070000;
+        public static final int menu_settings=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int acercade=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f060000;
--- src/org/example/asteroides/Asteroides.java
+++ src/org/example/asteroides/Asteroides.java
@@ -2,7 +2,9 @@ package org.example.asteroides;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Asteroides extends Activity {
 
@@ -17,6 +19,12 @@ public class Asteroides extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void lanzarAcercaDe(View view) {
+		Intent i = new Intent(this, AcercaDe.class);
+
+		startActivity(i);
 	}
 
 }
