d0f60f7c0695979abf198c42a23e722f871716fc
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-03-17 20:51:41 +0100

    Add an activity to show the score board

--- gen/org/example/asteroides/R.java
+++ gen/org/example/asteroides/R.java
@@ -31,6 +31,7 @@ public final class R {
     public static final class layout {
         public static final int acercade=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int puntuaciones=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f080000;
---
+++ src/org/example/asteroides/Puntuaciones.java
@@ -0,0 +1,21 @@
+package org.example.asteroides;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class Puntuaciones extends ListActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.puntuaciones);
+
+		setListAdapter(
+			new ArrayAdapter<String>(
+				this, android.R.layout.simple_list_item_1,
+				Asteroides.almacen.listaPuntuaciones(10)));
+	}
+
+}
