c654f23baca829e26125771cfd10d763a2e05ba8
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-03-18 08:50:43 +0100

    supress warning for Deprecation

--- src/org/example/asteroides/Preferencias.java
+++ src/org/example/asteroides/Preferencias.java
@@ -5,6 +5,7 @@ import android.preference.PreferenceActivity;
 
 public class Preferencias extends PreferenceActivity {
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
