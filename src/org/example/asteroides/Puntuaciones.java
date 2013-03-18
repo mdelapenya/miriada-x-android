f93e3e9cc2e62c55bf0d456c79ac7867d447b5b1
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-03-18 09:38:56 +0100

    Add listener to click event in list items

--- src/org/example/asteroides/Puntuaciones.java
+++ src/org/example/asteroides/Puntuaciones.java
@@ -2,6 +2,9 @@ package org.example.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Puntuaciones extends ListActivity {
 
@@ -15,4 +18,17 @@ public class Puntuaciones extends ListActivity {
 			new MiAdaptador(this, Asteroides.almacen.listaPuntuaciones(10)));
 	}
 
+	@Override
+	protected void onListItemClick(
+		ListView listView, View view, int position, long id) {
+
+		super.onListItemClick(listView, view, position, id);
+
+		Object o = getListAdapter().getItem(position);
+
+		Toast.makeText(
+			this,
+			"Selección: " + Integer.toString(position) + " - " + o.toString(),
+			Toast.LENGTH_LONG).show();
+	}
 }
