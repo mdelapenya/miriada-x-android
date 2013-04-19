995032f7ddf1b9ae60c443859c95615ec1250e41
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-04-19 21:34:05 +0200

    Start the broadcast when an SMS is received

---
+++ src/org/example/asteroides/SMSReceiver.java
@@ -0,0 +1,27 @@
+package org.example.asteroides;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+
+		if (action.equals("android.provider.Telephony.SMS_RECEIVED")) {
+			Bundle extras = intent.getExtras();
+
+			if (extras != null) {
+				Intent intencion = new Intent(context, AcercaDe.class);
+
+				intencion.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+				context.startActivity(intent);
+			}
+		}
+	}
+
+}
